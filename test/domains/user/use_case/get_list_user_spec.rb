c73e8124104c9327fe84bd3812f8e7caf9749be8
Hammam Firdaus <hammamxalf@gmail.com>
2023-04-23 09:28:33 +0700

    fix: rspec user get list

--- test/domains/user/use_case/get_list_user_spec.rb
+++ test/domains/user/use_case/get_list_user_spec.rb
@@ -9,8 +9,8 @@ RSpec.describe Users::UseCases::GetListUser, type: :use_case do
     create_list(:user, 100, organization: model_organization)
   end
   it 'should be true when get list user' do
-    pagy, users = described_class.new({}).call
-    expect(pagy.vars).to include(count: 100, page: 1, items: 20)
+    pagy, users = described_class.new(params: {}, organization_id: model_organization.id).call
+    expect(pagy.vars).to include(count: 100, page: 1, items: 10)
     expect(users.first.organization).to eq(model_organization)
   end
 end

aa2692d32c6d23db34c2e23b4e1682bec628319d
Hammam Firdaus <hammamxalf@gmail.com>
2023-04-25 18:38:58 +0700

    fix: dashboard url after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
-    dashboard_index_path
+    dashboards_url
   end
 
   def handle_result(redirect_url, result)
